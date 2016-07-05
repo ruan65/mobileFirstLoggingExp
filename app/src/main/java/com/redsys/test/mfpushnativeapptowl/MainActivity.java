905499eda0416534adfd167c17c53cbef65187b1
Andrey <ruan65@gmail.co>
2016-07-05 11:58:44 +0300

    initial

---
+++ app/src/main/java/com/redsys/test/mfpushnativeapptowl/MainActivity.java
@@ -0,0 +1,13 @@
+package com.redsys.test.mfpushnativeapptowl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
